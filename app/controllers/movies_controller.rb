6fc519d257f6f5f73e06d125707054e58d3f0672
supertrunkes <supertrunkes@gmail.com>
2017-10-26 15:53:02 -0700

    worked on show pages cleaned up some files added listmovies controller, styling

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,10 @@ class MoviesController < ApplicationController
   def index
     response = HTTParty.get('https://api.themoviedb.org/3/search/movie?api_key=7c63b5b2b830539c1cc4b3954b6c8982&language=en-US&page=1&include_adult=false&query=' + params[:q])
     @movies = response
+    @user = current_user
+    if user_signed_in?
+        @users_lists = List.where(user_id: current_user.id)
+      end
   end
 
   def show
